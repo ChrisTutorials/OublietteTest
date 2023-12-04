2fd84eafc6f8a5d9935db3d4ff040b3172dd5c4e
Chris' Tutorials <christutorialsyt@gmail.com>
2023-12-04 08:05:36 +0700

    fixed add to inventory adding to 3 arrays

--- 05Resources/Inventory/Inventory.gd
+++ 05Resources/Inventory/Inventory.gd
@@ -5,10 +5,10 @@ export (String) var player_name = ""
 export (int) var Money = 0 #aka coins
 export (int) var Potions = 0
 export (int) var Keys = 0
-export (Array, Resource) var Collected = []
-export (Array, Resource) var Items = [] #unused atm
+export (Array) var Collected
+export (Array) var Items #unused atm
 export (int) var playerlevel = 1
 export (int) var current_health = 10
 export (int) var max_health = 10
 export (int) var damage = 2
-export (Array, Resource) var StatePrizes = []
+export (Array) var StatePrizes
